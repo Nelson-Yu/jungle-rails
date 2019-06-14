15a7ec6735a14fc649552df4bd32936cfcd6e7a3
Nelson-Yu <nelson.tm.yu@gmail.com>
2019-06-14 15:34:21 -0700

    Added login requirement to post a review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  before_filter :authorize
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.review.create(review_params)
+    @review.user = current_user
+    @review.save
+    redirect_to product_url(@product)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
+end
