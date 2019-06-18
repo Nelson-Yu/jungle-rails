abe79895b523a5234d449873a8d411398816db4b
Nelson Yu <nelson.tm.yu@gmail.com>
2019-06-18 15:17:46 -0700

    Edited Feature Tests

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -6,19 +6,21 @@ RSpec.feature "Visitions click on add to cart button on home page", type: :featu
   before :each do 
     @category = Category.create! name: 'Apparel'
 
-    @category.products.create!(
-      name: Faker::Hipster.sentence(3),
-      description: Faker::Hipster.paragraph(4),
-      image: open_asset('apparel1.jpg'),
-      quantity: 10,
-      price: 64.99
-    )
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
   end
 
   scenario "They see the My Cart counter go up" do
     # ACT
     visit root_path
-    click_on 'Add'
+    first(:button, 'Add').click
 
     # VERIFY & DEBUG
     expect(page).to have_content('My Cart (1)')
--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -6,20 +6,22 @@ RSpec.feature "Visitor clicks on product on home page", type: :feature, js: true
   before :each do 
     @category = Category.create! name: 'Apparel'
 
-    @category.products.create!(
-      name: Faker::Hipster.sentence(3),
-      description: Faker::Hipster.paragraph(4),
-      image: open_asset('apparel1.jpg'),
-      quantity: 10,
-      price: 64.99
-    )
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
   end
 
   scenario "They see the product, they clicked on" do
     # ACT
     visit root_path
-    click_on 'Details »'
-
+    first(:link, 'Details').click
+    
     # VERIFY & DEBUG
     expect(page).to have_css '.products-show', count: 1
     save_screenshot
